6b6651c926595b9ee1074fe28c5d357e09fecd1e
edision <boywujch@163.com>
2016-12-15 14:39:14 +0800

    清理reducers\index.ts代码

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,19 +1,12 @@
 import { combineReducers } from "redux-immutablejs";
-import * as Redux from "redux";
-import * as Immutable from "immutable";
+
 // reducers
 import counter from "./counter";
 import routing from './routing';
-
-
 
 const rootReducer = {
     counter,
     routing
 };
-
-// export type RootReducer = {
-//     counter: CounterImmutableState
-// }
 
 export default combineReducers(rootReducer);
